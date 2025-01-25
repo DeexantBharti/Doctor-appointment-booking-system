abc2c62ffd5d8e69706d0abdd19a751013006219
DeexantBharti <deexantbharti4321@gmail.com>
2025-01-25 11:17:49 +0530

    Specaility section at home page.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header.jsx'
+import SpecialityMenu from '../components/SpecialityMenu.jsx'
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <SpecialityMenu />
     </div>
   )
 }
