df7dd3acbfff8288feebcb8cc49cf1ff82a558e5
DeexantBharti <deexantbharti4321@gmail.com>
2025-01-25 12:49:37 +0530

    TopDoctors functionality at home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header.jsx'
 import SpecialityMenu from '../components/SpecialityMenu.jsx'
+import TopDoctors from '../components/TopDoctors.jsx'
 
 export default function Home() {
   return (
     <div>
       <Header />
       <SpecialityMenu />
+      <TopDoctors />
     </div>
   )
 }
