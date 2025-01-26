b887cd56bd9b28c275a2b6181982e87c84e9b7f8
DeexantBharti <deexantbharti4321@gmail.com>
2025-01-26 23:29:11 +0530

    about page creation.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home.jsx'
 import Doctors from './pages/Doctors.jsx'
 import Login from './pages/Login.jsx'
 import About from './pages/About.jsx'
-import Contacts from './pages/Contacts.jsx'
+import Contact from './pages/Contact.jsx'
 import MyProfile from './pages/MyProfile.jsx'
 import MyAppointments from './pages/MyAppointments.jsx'
 import Appointment from './pages/Appointment.jsx'
@@ -21,7 +21,7 @@ export default function App() {
       <Route path='/doctors/:speciality' element={<Doctors/>} />
       <Route path='/login' element={<Login/>} />
       <Route path='/about' element={<About/>} />
-      <Route path='/contact' element={<Contacts/>} />
+      <Route path='/contact' element={<Contact/>} />
       <Route path='/my-profile' element={<MyProfile/>} />
       <Route path='/my-appointment' element={<MyAppointments/>} />
       <Route path='/appointment/:docId' element={<Appointment/>} />
---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { assets } from '../assets/assets.js'
+
+export default function Contact() {
+  return (
+    <div>
+        <div className='text-center text-2xl pt-10 text-gray-500'>
+            <p>CONTACT <span className='text-gray-700 font-semibold'>US</span></p>
+        </div>
+        <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+            <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+            <div className='flex flex-col justify-center items-start gap-6'>
+                <p className='font-semibold text-lg text-gray-600'>Our Office</p>
+                <p className='text-gray-50'>54709 willms station <br />suite 350, washington USA.</p>
+                <p className='text-gray-500'>Tel: (415) 555-0132 <br /> Email: bhartideexant1234@gmail.com</p>
+                <p className='font-semibold text-lg text-gray-600'>Carrears at Prescripto</p>
+                <p className='text-gray-500'>Learns more about teams and job openings</p>
+                <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white translate-all duration-500 cursor-pointer'>Explore Jobs</button>
+            </div>
+        </div>
+    </div>
+  )
+}
