f1b6ff989b5270a4dd67ae831ee57d1e91bccc8a
DeexantBharti <deexantbharti4321@gmail.com>
2025-01-27 18:26:02 +0530

    connection of database and cloudinary status.

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -11,7 +11,7 @@ export default function Contact() {
             <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
             <div className='flex flex-col justify-center items-start gap-6'>
                 <p className='font-semibold text-lg text-gray-600'>Our Office</p>
-                <p className='text-gray-50'>54709 willms station <br />suite 350, washington USA.</p>
+                <p className='text-gray-500'>54709 willms station <br />suite 350, washington USA.</p>
                 <p className='text-gray-500'>Tel: (415) 555-0132 <br /> Email: bhartideexant1234@gmail.com</p>
                 <p className='font-semibold text-lg text-gray-600'>Carrears at Prescripto</p>
                 <p className='text-gray-500'>Learns more about teams and job openings</p>
