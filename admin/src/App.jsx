a3524faae0d77478948c240da929dada737fbc19
DeexantBharti <deexantbharti4321@gmail.com>
2025-02-06 18:43:31 +0530

    full doctor adding functionality and admin navbar page.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Login from './pages/Login.jsx'
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+import { AdminContext } from './context/AdminContext.jsx';
+import Navbar from './components/Navbar.jsx'
+
+
 
 export default function App() {
-  return (
+  const {aToken} = useContext(AdminContext)
+  return aToken ? (
        
-    <div>
-      <Login />
+    <div className='bg-[#F8F9FD]'>
+
+      <ToastContainer />  
+      <Navbar />
     </div>
 
+  )
+  :
+  (
+    <>
+    <Login />
+    <ToastContainer />
+    </>
   )
 }
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
   <AdminContextProvider>
     <DoctorContextProvider>
       <AppContextProvider>
-          <App />
+          <App /> 
       </AppContextProvider>
     </DoctorContextProvider>
   </AdminContextProvider>
