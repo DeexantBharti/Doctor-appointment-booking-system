fb52d18561059722d28e74ed44d8ac6818110bb3
DeexantBharti <deexantbharti4321@gmail.com>
2025-02-10 02:18:47 +0530

    Availability status functionality.

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -26,9 +26,26 @@ const backendUrl = import.meta.env.VITE_BACKEND_URL
     }
   }
 
+   const changeAvailability = async (docId) =>{
+     
+     try {
+        const {data} = await axios.post(backendUrl+'/api/admin/change-availability',{docId},{headers:{aToken}})
+        if(data.success){
+            toast.success(data.message)
+            getAllDoctors()
+        }
+        else{
+          toast.error(data.message)
+        }
+     } catch (error) {
+        toast.error(error.message)
+     }
+
+   }      
+
     const value = {
        aToken,setAToken,
-       backendUrl,doctors,getAllDoctors
+       backendUrl,doctors,getAllDoctors,changeAvailability
     }
     return (
         <AdminContext.Provider value={value}>
