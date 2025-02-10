b9273e48446c159dbfbf329571977188f1509b8b
DeexantBharti <deexantbharti4321@gmail.com>
2025-02-10 11:03:57 +0530

    API for user registration.

---
+++ api/controllers/userController.js
@@ -0,0 +1,42 @@
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+import userModel from '../models/userModel.js'
+import jwt from 'jsonwebtoken'
+// api to resgister user
+
+const registerUser = async(req,res) => {
+    try {
+        const {name, email,password} = req.body
+        if(!name || !password || !email){
+            return res.json({success:false,message:"missing details"})
+        }
+        // validating email format
+        if(!validator.isEmail(email)){
+            return res.json({success:false,message:"enter a valid email"});
+        }
+        // validating strong password
+        if(password.length < 8 ){
+            return res.json({success:false,message:"enter a strong password"})
+        }
+
+        // hasing user password 
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+        const userData = {
+            name, email,password:hashedPassword
+        }
+        
+        const newUser = new userModel(userData)
+        const user = await newUser.save()
+
+        // create one token so user can login
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+        res.json({success:true, token})
+
+    } catch (error) {
+        console.log(error)
+        res.json({suceess:false,message:error.message})
+    }
+}
+
+export {registerUser}
--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import adminRouter from './Routes/adminRoute.js';
 import doctorRouter from './Routes/doctorRoute.js';
+import userRouter from './Routes/userRoute.js';
 
 
 
@@ -23,7 +24,7 @@ app.use(cors())
 // api endpoint
 app.use('/api/admin',adminRouter)
 app.use('/api/doctor',doctorRouter)
-
+app.use('/api/user',userRouter)
 //localhost:4000/api/admin/add-doctor
 
 
