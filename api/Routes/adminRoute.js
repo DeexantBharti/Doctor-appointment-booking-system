6669cafbdf53856c5392e2d8600caada0e943bec
DeexantBharti <deexantbharti4321@gmail.com>
2025-02-14 13:09:58 +0530

    complete admin portal with dashboard page.

--- api/Routes/adminRoute.js
+++ api/Routes/adminRoute.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 
 
-import { addDoctor ,allDoctors,appointmentCancel,appointmentsAdmin,loginAdmin } from '../controllers/adminController.js'
+import { addDoctor ,adminDashboard,allDoctors,appointmentCancel,appointmentsAdmin,loginAdmin } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
 import authAdmin from '../middlewares/authAdmin.js'
 import { changeAvailability } from '../controllers/doctorController.js'
@@ -16,5 +16,6 @@ adminRouter.post('/all-doctors',authAdmin, allDoctors)
 adminRouter.post('/change-availability',authAdmin,changeAvailability)
 adminRouter.get('/appointments',authAdmin,appointmentsAdmin)
 adminRouter.post('/cancel-appointment',authAdmin,appointmentCancel)
+adminRouter.get('/dashboard',authAdmin,adminDashboard)
 
 export default adminRouter
